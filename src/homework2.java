1153b5a057888ebff8a8a33dda71ad5e86266886
technischeman <79165779+technischeman@users.noreply.github.com>
2021-04-09 23:21:03 +0200

    Update homework2.java

--- src/homework2.java
+++ src/homework2.java
@@ -1,14 +1,14 @@
 public class homework2 {
     public static void main(String[] args) {
         int[]intarray=new int[]{5,6,7};
-        int[]neuaarra=new int[4];
+        int[]newArray=new int[4];
         for (int i = 0; i < intarray.length ; i++) {
-            neuaarra[i]=intarray[i];
+            newArray[i]=intarray[i];
 
         }
-        neuaarra[3]=8;
-        for (int i = 0; i < neuaarra.length ; i++) {
-            System.out.println("yeniler"+neuaarra[i]);
+        newArray[3]=8;
+        for (int i = 0; i < newArray.length ; i++) {
+            System.out.println("yeniler"+newArray[i]);
 
 
         }
