19c3c2273502cdb0207229260b1a4fc970344e54
technischeman <79165779+technischeman@users.noreply.github.com>
2021-04-09 23:23:00 +0200

    Update homework3.java

--- src/homework3.java
+++ src/homework3.java
@@ -1,13 +1,13 @@
 public class homework3 {
     public static void main(String[] args) {
-        int[]eleman5={1,9,3};
+        int[]elements={1,9,3};
         int[]neu=new int[2];
-        for (int i = 0; i < eleman5.length-1 ; i++) {
-           neu[i]=eleman5[i];
+        for (int i = 0; i < elements.length-1 ; i++) {
+           neu[i]=elements[i];
 
         }
         for (int i = 0; i < neu.length ; i++) {
-            System.out.println("yeni"+neu[i]);
+            System.out.println("new"+neu[i]);
 
 
         }
